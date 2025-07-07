8738926fb98a9df5b7404af25e1b16046e320ae8
sandipsky <sandipshakya75@gmail.com>
2025-07-07 18:12:33 +0545

    changes

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [MatSidenavModule, Header],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
