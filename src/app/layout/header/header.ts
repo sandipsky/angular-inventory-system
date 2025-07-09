d1e61fb927b1c5b4ccd63c8a2dbfd768cbd601c8
sandipsky <sandipshakya75@gmail.com>
2025-07-09 12:45:20 +0545

    normal side

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  onMenuClick = output<void>();
 
+  emitClick() {
+    this.onMenuClick.emit();
+  }
 }
---
+++ src/app/layout/sidebar/sidebar.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [MatIconModule],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss'
+})
+export class Sidebar {
+  navData = [
+    {
+      name: "Dashboard",
+      icon: "dashboard",
+      url: "dashboard"
+    },
+    {
+      name: "Inventory",
+      icon: "inventory",
+      url: "inventory"
+    },
+    {
+      name: "billing",
+      icon: "request_page",
+      url: "billing"
+    },
+    {
+      name: "accounting",
+      icon: "difference",
+      url: "accounting"
+    },
+    {
+      name: "users",
+      icon: "group",
+      url: "users"
+    },
+    {
+      name: "masters",
+      icon: "dashboard_customize",
+      url: "masters"
+    },
+    {
+      name: "reports",
+      icon: "assessment",
+      url: "reports"
+    },
+    {
+      name: "Settings",
+      icon: "settings",
+      url: "settings"
+    }
+  ]
+}
